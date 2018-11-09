2e3cc93d74e7c939bb29a9333ed0798a1d67a8e5
Zilun Peng <zilunpeng@gmail.com>
2018-11-09 17:18:17 -0500

    add main func

--- build_drawings.py
+++ build_drawings.py
@@ -4,12 +4,11 @@ import numpy as np
 import ast
 import draw_lines
 
-num_img_to_show = 64
-data = pd.read_csv('train_simplified/fence.csv')
-num_imgs = data.shape[0]
-data = data.iloc[np.random.choice(num_imgs,size=num_img_to_show,replace=False),:]
-data['drawing'] = data['drawing'].apply(ast.literal_eval)
-data = data['drawing']
+def get_sample_imgs(num_img_to_show, data):
+    num_imgs = data.shape[0]
+    data = data.iloc[np.random.choice(num_imgs, size=num_img_to_show, replace=False), :]
+    data['drawing'] = data['drawing'].apply(ast.literal_eval)
+    return data['drawing']
 
 def build_drawing(drawing,size):
     img = np.zeros((size,size))
@@ -19,17 +18,24 @@ def build_drawing(drawing,size):
             img[x_coords, y_coords] = 1
     return img
 
-import matplotlib.pyplot as plt
-n = int(np.sqrt(num_img_to_show))
-fig, axs = plt.subplots(nrows=n, ncols=n, sharex=True, sharey=True, figsize=(12, 12))
-for i in range(num_img_to_show):
-    ax = axs[i // n, i % n]
-    ax.imshow(build_drawing(data.iloc[i],256), cmap=plt.cm.gray)
-    ax.axis('off')
-    # plt.imshow(build_drawing(data.iloc[i],256), cmap=plt.cm.gray)
-    # plt.axis('off')
-    # plt.show()
 
-plt.tight_layout()
-fig.savefig('samples.png', dpi=300)
-plt.show()
+def plot_sample_imgs(num_img_to_show,data):
+    import matplotlib.pyplot as plt
+    n = int(np.sqrt(num_img_to_show))
+    fig, axs = plt.subplots(nrows=n, ncols=n, sharex=True, sharey=True, figsize=(12, 12))
+    for i in range(num_img_to_show):
+        ax = axs[i // n, i % n]
+        ax.imshow(build_drawing(data.iloc[i],256), cmap=plt.cm.gray)
+        ax.axis('off')
+        # plt.imshow(build_drawing(data.iloc[i],256), cmap=plt.cm.gray)
+        # plt.axis('off')
+        # plt.show()
+    plt.tight_layout()
+    fig.savefig('samples.png', dpi=300)
+    plt.show()
+
+if __name__ == "__main__":
+    num_img_to_show = 64
+    data = pd.read_csv('train_simplified/fence.csv')
+    data = get_sample_imgs(num_img_to_show, data)
+    plot_sample_imgs(num_img_to_show,data)
