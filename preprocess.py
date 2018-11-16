d23bd03c51ad7824d43a0eecfa55e3ee9ec22448
Zilun Peng <zilunpeng@gmail.com>
2018-11-16 11:36:02 -0500

    skip if cv folder already exists

--- preprocess.py
+++ preprocess.py
@@ -14,24 +14,27 @@ def get_num_data_per_category(dir_name, is_printing):
 def train_test_split(dir_name,num_folds):
     kf = KFold(n_splits=num_folds,shuffle=True)
     for filename in os.listdir(dir_name):
-        path = os.path.join(dir_name,filename)
-        data = pd.read_csv(path)
-        drawings = list(map(ast.literal_eval, data['drawing']))
-        data['drawing'] = drawings
-        data = data.drop(columns=['countrycode','key_id','timestamp',])
-        if data.shape[0] > 0:
-            cat_name = os.path.splitext(filename)[0]
-            if not os.path.exists(os.path.join('cv_simplified/',cat_name)):
+        cat_name = os.path.splitext(filename)[0]
+        print('creating cv files for ' + cat_name)
+        if not os.path.exists(os.path.join('cv_simplified/', cat_name)):
+            path = os.path.join(dir_name,filename)
+            data = pd.read_csv(path)
+            drawings = list(map(ast.literal_eval, data['drawing']))
+            data['drawing'] = drawings
+            data = data.drop(columns=['countrycode','key_id','timestamp',])
+            if data.shape[0] > 0:
                 os.makedirs(os.path.join('cv_simplified/',cat_name))
-            i = 1
-            for tr_inds, val_inds in kf.split(data):
-                tr_data = data.iloc[tr_inds, :]
-                tr_data = tr_data.reset_index()
-                val_data = data.iloc[val_inds, :]
-                val_data = val_data.reset_index()
-                cv_dir_name = 'cv_simplified/'+cat_name+'/'
-                tr_data.to_pickle(path=cv_dir_name+"tr"+str(i)+".pkl")
-                val_data.to_pickle(path=cv_dir_name+"val"+str(i)+".pkl")
-                i += 1
+                i = 1
+                for tr_inds, val_inds in kf.split(data):
+                    tr_data = data.iloc[tr_inds, :]
+                    tr_data = tr_data.reset_index()
+                    val_data = data.iloc[val_inds, :]
+                    val_data = val_data.reset_index()
+                    cv_dir_name = 'cv_simplified/'+cat_name+'/'
+                    tr_data.to_pickle(path=cv_dir_name+"tr"+str(i)+".pkl")
+                    val_data.to_pickle(path=cv_dir_name+"val"+str(i)+".pkl")
+                    i += 1
+        else:
+            print(cat_name + 'already exists')
 
 train_test_split('train_simplified',10)
