f45fc13390f49440ae39fa14cb07bac5c5b21ff7
Zilun Peng <zilunpeng@gmail.com>
2018-11-15 21:55:56 -0500

    store pickle files

--- preprocess.py
+++ preprocess.py
@@ -1,3 +1,4 @@
+import ast
 import os
 import pandas as pd
 import numpy as np
@@ -15,6 +16,9 @@ def train_test_split(dir_name,num_folds):
     for filename in os.listdir(dir_name):
         path = os.path.join(dir_name,filename)
         data = pd.read_csv(path)
+        drawings = list(map(ast.literal_eval, data['drawing']))
+        data['drawing'] = drawings
+        data = data.drop(columns=['countrycode','key_id','timestamp',])
         if data.shape[0] > 0:
             cat_name = os.path.splitext(filename)[0]
             if not os.path.exists(os.path.join('cv_simplified/',cat_name)):
@@ -22,12 +26,12 @@ def train_test_split(dir_name,num_folds):
             i = 1
             for tr_inds, val_inds in kf.split(data):
                 tr_data = data.iloc[tr_inds, :]
+                tr_data = tr_data.reset_index()
                 val_data = data.iloc[val_inds, :]
+                val_data = val_data.reset_index()
                 cv_dir_name = 'cv_simplified/'+cat_name+'/'
-                tr_data.to_csv(path_or_buf=cv_dir_name+"tr"+str(i)+".csv",columns=['drawing','recognized','word'],index_label='ind')
-                val_data.to_csv(path_or_buf=cv_dir_name+"val"+str(i)+".csv",columns=['drawing','recognized','word'],index_label='ind')
+                tr_data.to_pickle(path=cv_dir_name+"tr"+str(i)+".pkl")
+                val_data.to_pickle(path=cv_dir_name+"val"+str(i)+".pkl")
                 i += 1
 
-#temp = pd.read_csv('cv_simplified/airplane/val1.csv',index_col='ind')
 train_test_split('train_simplified',10)
-#get_num_data_per_category('train_simplified',True)
