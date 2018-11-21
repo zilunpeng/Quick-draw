55336811dbe381e2a92fd9e9fb45ae1d705cef30
Zilun Peng <zilunpeng@gmail.com>
2018-11-20 21:03:37 -0500

    only look at samples once in every epoch

--- sag4crf_training.py
+++ sag4crf_training.py
@@ -7,13 +7,12 @@ from scipy.special import logsumexp
 
 class sag4crf:
 
-    def __init__(self, data_dir, fold_num, regularization_param, step_size, maximum_iteration, err_tolerance, maximum_iteration_on_one_category_multiplier):
+    def __init__(self, data_dir, fold_num, regularization_param, step_size, maximum_iteration, err_tolerance):
         self.data_dir = data_dir
         self.reg_lam = regularization_param
         self.max_iter = maximum_iteration
         self.delta = err_tolerance
         self.fold_num = fold_num
-        self.max_iter_multiplier = maximum_iteration_on_one_category_multiplier
         self.alpha = step_size
 
         self.init_all_cats(data_dir)
@@ -39,10 +38,8 @@ class sag4crf:
         self.cur_tr_data_path = self.cur_cat_path + '/tr' + str(self.fold_num) + '.pkl'
         self.cur_tr_data_fold = pd.read_pickle(self.cur_tr_data_path)
         self.cur_tr_fold_size = self.cur_tr_data_fold.shape[0]
-        self.cur_tr_fold_counter = 0
-        self.cur_tr_fold_sample_freq_counter = np.zeros(self.cur_tr_fold_size)
-        self.max_iter_on_cat = self.max_iter_multiplier * self.cur_tr_fold_size
-        self.probs = (np.zeros(851968) if self.cat_visit_freq[self.cur_cat] == 0 else np.load(self.cur_cat_path+'/probs.npy'))
+        self.cur_tr_fold_seq = np.random.permutation(self.cur_tr_fold_size)
+        self.probs = (np.zeros(self.cur_tr_fold_size) if self.cat_visit_freq[self.cur_cat] == 0 else np.load(self.cur_cat_path+'/probs.npy'))
         self.cat_visit_freq[self.cur_cat] += 1
         self.init_val_fold()
 
@@ -63,15 +60,12 @@ class sag4crf:
         self.probs[data_id] = new_prob
         return d
 
-    def custom_random_sampler(self):
-        data_id = np.random.randint(self.cur_tr_fold_size)
+    def custom_random_sampler(self, iter):
+        data_id = self.cur_tr_fold_seq[iter]
         x_i = self.cur_tr_data_fold.loc[data_id,'drawing']
-        y_i = self.cur_cat
-        self.cur_tr_fold_counter += 1
-        if self.cat_visit_freq[self.cur_cat] == 1 and self.cur_tr_fold_sample_freq_counter[data_id] == 0:
+        if self.cat_visit_freq[self.cur_cat] == 1:
             self.tot_data_seen += 1
-            self.cur_tr_fold_sample_freq_counter[data_id] = 1
-        return data_id,x_i, y_i
+        return data_id,x_i
 
     def get_val_err(self):
         val_err = 0
@@ -85,25 +79,29 @@ class sag4crf:
 
     def sag_training(self):
         iter = 0
+        epoch = 0
         d = np.zeros(851968)
         then = time.time()
         print('start training')
         while iter<self.max_iter:
-            data_id,x_i,y_i = self.custom_random_sampler()
+            data_id,x_i = self.custom_random_sampler(iter)
             feat_i = build_feature.set_feature_mat(x_i,256)
             d = self.compute_d(d,data_id,feat_i)
             w = (1-self.alpha*self.reg_lam)*self.weights[self.cur_cat,:] - (self.alpha/self.tot_data_seen)*d
             self.weights[self.cur_cat, :] = w
 
-            if self.cur_tr_fold_counter > self.max_iter_on_cat:
+            if iter % 200 == 0: print('iter=%d' % (iter) + ' prob=%.7f' % (self.probs[data_id]))
+            iter += 1
+            if self.iter >= self.cur_tr_fold_size:
                 now = time.time()
                 print('finished training on category ' + self.cat_names[self.cur_cat] + '. Took %.2f'%(now-then) + 'seconds. Start validating.')
                 val_err = self.get_val_err()
-                print('iter=%d. trained on category '%(iter) + self.cat_names[self.cur_cat] + '. NLL on validation set is %.5f'%(val_err))
+                print('epoch=%d. trained on category '%(epoch) + self.cat_names[self.cur_cat] + '. NLL on validation set is %.5f'%(val_err))
                 self.update_category()
-                then = now
-                iter += 1
+                then = time.time()
+                iter = 0
+                epoch += 1
         return w
 
-crf = sag4crf(data_dir='cv_simplified',fold_num=1,regularization_param=0.001,step_size=0.000001,maximum_iteration=3*340,err_tolerance=0.00001,maximum_iteration_on_one_category_multiplier=1)
+crf = sag4crf(data_dir='cv_simplified',fold_num=1,regularization_param=0.001,step_size=0.0001,maximum_iteration=3*340,err_tolerance=0.00001)
 crf.sag_training()
