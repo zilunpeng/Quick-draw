08fc49a022a264b6e929d8f13b6f719615a99aba
Zilun Peng <zilunpeng@gmail.com>
2018-11-26 20:21:38 -0500

    change location of print statement

--- preprocess.py
+++ preprocess.py
@@ -36,9 +36,9 @@ def train_test_split(dir_name,num_folds,num_workers,tr_data_pct):
             data = np.zeros((data_size,851968), dtype=np.bool)
             inds = range(data_size)[0::100]
             for i in inds:
+                print('starting' + str(i))
                 with Pool(num_workers) as p:
                     data[i:i+100] = p.map(build_feature.set_feature_mat, drawings[i:i+100])
-                    print('starting' + str(i))
             print('finished preprocessing')
             data = scipy.sparse.csc_matrix(data)
             print(sys.getsizeof(data))
