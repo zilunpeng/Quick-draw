c7dd54b4888d9d0467c99d262924397b2b289e72
Zilun Peng <zilunpeng@gmail.com>
2018-11-28 12:30:03 -0500

    convert build feature to a class

--- build_feature.py
+++ build_feature.py
@@ -1,106 +1,98 @@
 import pandas as pd
 import numpy as np
 import ast
-import scipy.sparse
+import tensorflow as tf
 
-def find_row_col_inds(data):
-    row_inds = []
-    col_inds = []
-    for stroke in data:
-        row_inds.extend(stroke[0])
-        col_inds.extend(stroke[1])
-    return np.array(row_inds), np.array(col_inds)
+class build_feature():
+    def __init__(self, writing_path):
+        self.writing_path = writing_path
 
-def get_up_node_coords(x_coords,y_coords):
-    return x_coords,y_coords-1
+    def find_row_col_inds(data):
+        row_inds = []
+        col_inds = []
+        for stroke in data:
+            row_inds.extend(stroke[0])
+            col_inds.extend(stroke[1])
+        return np.array(row_inds), np.array(col_inds)
 
-def get_down_node_coords(x_coords,y_coords):
-    return x_coords,y_coords+1
+    def get_up_node_coords(x_coords, y_coords):
+        return x_coords, y_coords - 1
 
-def get_left_node_coords(x_coords,y_coords):
-    return x_coords-1,y_coords
+    def get_down_node_coords(x_coords, y_coords):
+        return x_coords, y_coords + 1
 
-def get_right_node_coords(x_coords,y_coords):
-    return x_coords+1,y_coords
+    def get_left_node_coords(x_coords, y_coords):
+        return x_coords - 1, y_coords
 
-def get_up_left_node_coords(x_coords,y_coords):
-    return x_coords-1,y_coords-1
+    def get_right_node_coords(x_coords, y_coords):
+        return x_coords + 1, y_coords
 
-def get_up_right_node_coords(x_coords,y_coords):
-    return x_coords+1,y_coords-1
+    def get_up_left_node_coords(x_coords, y_coords):
+        return x_coords - 1, y_coords - 1
 
-def get_down_left_node_coords(x_coords,y_coords):
-    return x_coords-1,y_coords+1
+    def get_up_right_node_coords(x_coords, y_coords):
+        return x_coords + 1, y_coords - 1
 
-def get_down_right_node_coords(x_coords,y_coords):
-    return x_coords+1,y_coords+1
+    def get_down_left_node_coords(x_coords, y_coords):
+        return x_coords - 1, y_coords + 1
 
-def get_valid_coord(x_coord, y_coord, size):
-    valid_x_coord = np.logical_and(x_coord>=0, x_coord<size)
-    valid_y_coord = np.logical_and(y_coord>=0, y_coord<size)
-    valid_coord = np.logical_and(valid_x_coord, valid_y_coord)
-    return valid_coord
+    def get_down_right_node_coords(x_coords, y_coords):
+        return x_coords + 1, y_coords + 1
 
-def intersect_coords(x_coord, y_coord, x_coord_ngbr, y_coord_ngbr):
-    comm_x = np.in1d(x_coord, x_coord_ngbr)
-    comm_y = np.in1d(y_coord, y_coord_ngbr)
-    return np.logical_and(comm_x, comm_y)
+    def get_valid_coord(x_coord, y_coord, size):
+        valid_x_coord = np.logical_and(x_coord >= 0, x_coord < size)
+        valid_y_coord = np.logical_and(y_coord >= 0, y_coord < size)
+        valid_coord = np.logical_and(valid_x_coord, valid_y_coord)
+        return valid_coord
 
-def sub2ind(x_coord, y_coord, size):
-    return y_coord*size + x_coord
+    def intersect_coords(x_coord, y_coord, x_coord_ngbr, y_coord_ngbr):
+        comm_x = np.in1d(x_coord, x_coord_ngbr)
+        comm_y = np.in1d(y_coord, y_coord_ngbr)
+        return np.logical_and(comm_x, comm_y)
 
-def get_tt_ind(x, y, x_ngbr, y_ngbr, is_ngbr_center, size):
-    valid_ngbr = get_valid_coord(x_ngbr, y_ngbr, size)
-    true_ngbr = intersect_coords(x, y, x_ngbr, y_ngbr)
-    inds = np.logical_and(true_ngbr, valid_ngbr)
-    inds = sub2ind(x_ngbr[inds], y_ngbr[inds], size) if is_ngbr_center else sub2ind(x[inds], y[inds], size)
-    return inds, true_ngbr, valid_ngbr
+    def sub2ind(x_coord, y_coord, size):
+        return y_coord * size + x_coord
 
-def get_tf_ft_ind(x,y,x_ngbr,y_ngbr,true_ngbr, valid_ngbr, is_ngbr_center, size):
-    false_ngbr = np.logical_not(true_ngbr)
-    inds = np.logical_and(false_ngbr, valid_ngbr)
-    inds = sub2ind(x_ngbr[inds], y_ngbr[inds], size) if is_ngbr_center else sub2ind(x[inds], y[inds], size)
-    return inds
+    def get_tt_ind(x, y, x_ngbr, y_ngbr, is_ngbr_center, size, self):
+        valid_ngbr = self.get_valid_coord(x_ngbr, y_ngbr, size)
+        true_ngbr = self.intersect_coords(x, y, x_ngbr, y_ngbr)
+        inds = np.logical_and(true_ngbr, valid_ngbr)
+        inds = self.sub2ind(x_ngbr[inds], y_ngbr[inds], size) if is_ngbr_center else self.sub2ind(x[inds], y[inds], size)
+        return inds, true_ngbr, valid_ngbr
 
-def set_edge_feature(x, y, get_center_ngbr_coords, get_noncent_ngbr_coords, size):
-    tot_nodes = size*size
-    edge_feature_tt = np.zeros(tot_nodes)
-    edge_feature_tf = np.zeros(tot_nodes)
-    edge_feature_ft = np.zeros(tot_nodes)
-    x_cent, y_cent = get_center_ngbr_coords(x,y)
-    x_noncent, y_noncent = get_noncent_ngbr_coords(x,y)
+    def get_tf_ft_ind(x, y, x_ngbr, y_ngbr, true_ngbr, valid_ngbr, is_ngbr_center, size, self):
+        false_ngbr = np.logical_not(true_ngbr)
+        inds = np.logical_and(false_ngbr, valid_ngbr)
+        inds = self.sub2ind(x_ngbr[inds], y_ngbr[inds], size) if is_ngbr_center else self.sub2ind(x[inds], y[inds], size)
+        return inds
 
-    inds, true_ngbr, valid_ngbr = get_tt_ind(x,y,x_cent,y_cent,is_ngbr_center=True,size=size)
-    edge_feature_tt[inds] = 1
-    edge_feature_ft[get_tf_ft_ind(x,y,x_cent,y_cent,true_ngbr,valid_ngbr,is_ngbr_center=True,size=size)] = 1
+    def set_edge_feature(x, y, get_center_ngbr_coords, get_noncent_ngbr_coords, size, self):
+        tot_nodes = size * size
+        edge_feature_tt = np.zeros(tot_nodes)
+        edge_feature_tf = np.zeros(tot_nodes)
+        edge_feature_ft = np.zeros(tot_nodes)
+        x_cent, y_cent = get_center_ngbr_coords(x, y)
+        x_noncent, y_noncent = get_noncent_ngbr_coords(x, y)
 
-    inds, true_ngbr, valid_ngbr = get_tt_ind(x,y,x_noncent,y_noncent,is_ngbr_center=False,size=size)
-    edge_feature_tt[inds] = 1
-    edge_feature_tf[get_tf_ft_ind(x,y,x_noncent,y_noncent,true_ngbr,valid_ngbr,is_ngbr_center=False,size=size)] = 1
+        inds, true_ngbr, valid_ngbr = self.get_tt_ind(x, y, x_cent, y_cent, is_ngbr_center=True, size=size)
+        edge_feature_tt[inds] = 1
+        edge_feature_ft[self.get_tf_ft_ind(x, y, x_cent, y_cent, true_ngbr, valid_ngbr, is_ngbr_center=True, size=size)] = 1
 
-    return np.concatenate((edge_feature_tt, edge_feature_tf, edge_feature_ft))
+        inds, true_ngbr, valid_ngbr = self.get_tt_ind(x, y, x_noncent, y_noncent, is_ngbr_center=False, size=size)
+        edge_feature_tt[inds] = 1
+        edge_feature_tf[self.get_tf_ft_ind(x, y, x_noncent, y_noncent, true_ngbr, valid_ngbr, is_ngbr_center=False, size=size)] = 1
 
-def set_feature_mat(drawing):
-    size = 256
-    feature_node = np.zeros(size*size)
-    (x, y) = find_row_col_inds(drawing)
-    feature_node[sub2ind(x, y, size)] = 1
-    feature_h_edge = set_edge_feature(x,y,get_left_node_coords,get_right_node_coords,size)
-    feature_v_edge = set_edge_feature(x,y,get_up_node_coords,get_down_node_coords,size)
-    feature_nw_edge = set_edge_feature(x,y,get_up_left_node_coords,get_down_right_node_coords,size)
-    feature_ne_edge = set_edge_feature(x,y,get_up_right_node_coords,get_down_left_node_coords,size)
-    return np.array(np.concatenate((feature_node,feature_h_edge,feature_v_edge,feature_nw_edge,feature_ne_edge)),dtype=np.bool)
+        return np.concatenate((edge_feature_tt, edge_feature_tf, edge_feature_ft))
 
-if __name__ == "__main__":
-    data = pd.read_csv('train_simplified/fence.csv')
-    print('data size: ', data.shape)
-    data = data.iloc[0,:]
-    data = data['drawing']
-    data = ast.literal_eval(data)
-    feature = set_feature_mat(data)
-    print(feature.shape)
-# print('num non-zero: ', np.count_nonzero(feature))
-# sp_mat = scipy.sparse.csc_matrix(feature)
-# scipy.sparse.save_npz('sparse_feature_testing.npz',sp_mat)
-# testing_sp_mat = scipy.sparse.load_npz('sparse_feature_testing.npz')
-# print(testing_sp_mat)
+    def set_feature_mat(drawing, self):
+        size = 256
+        feature_node = np.zeros(size * size)
+        (x, y) = self.find_row_col_inds(drawing)
+        feature_node[self.sub2ind(x, y, size)] = 1
+        feature_h_edge = self.set_edge_feature(x, y, self.get_left_node_coords, self.get_right_node_coords, size)
+        feature_v_edge = self.set_edge_feature(x, y, self.get_up_node_coords, self.get_down_node_coords, size)
+        feature_nw_edge = self.set_edge_feature(x, y, self.get_up_left_node_coords, self.get_down_right_node_coords, size)
+        feature_ne_edge = self.set_edge_feature(x, y, self.get_up_right_node_coords, self.get_down_left_node_coords, size)
+        feature = np.where(np.concatenate((feature_node, feature_h_edge, feature_v_edge, feature_nw_edge, feature_ne_edge)))
+        np.savetxt(self.writing_path, feature)
+        # return np.array(np.concatenate((feature_node,feature_h_edge,feature_v_edge,feature_nw_edge,feature_ne_edge)),dtype=np.bool)
